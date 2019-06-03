563153a26fc1641c691ed78a22fef2ed08e55ff5
wmjaynes <will@jaynes.org>
2019-06-03 09:27:50 -0400

    Add 503 service down error page.

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <section class="hero is-white is-rounded">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Balance back
+                </h1>
+                <h2 class="subtitle">
+                    Service is currently unavailable. The dance will resume presently.
+                </h2>
+            </div>
+        </div>
+    </section>
+@endsection
