d51023830623256e80eb6eb232d4b5ac81202052
wmjaynes <will@jaynes.org>
2019-06-04 10:03:10 -0400

    Add Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+
+@if (env('APP_ENV') === 'prod')
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-29995131-3"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+
+            gtag('config', 'UA-29995131-3');
+        </script>
+    @endif
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -23,8 +40,9 @@
     </figure>
     <nav class="navbar">
         <div class="navbar-brand">
-            <a id="navbar-about" class="navbar-item" href="{{ route('about') }}">About</a>
-            <a id="navbar-dances" class="navbar-item" href="{{ route('dances') }}">Dances</a>
+            <a id="navbar-about" class="navbar-item" href="{{ route('about') }}">About</a> <a id="navbar-dances"
+                                                                                              class="navbar-item"
+                                                                                              href="{{ route('dances') }}">Dances</a>
         </div>
     </nav>
 
@@ -33,10 +51,10 @@
 </div>
 
 <script>
-    if(window.location.href.indexOf("about") > -1) {
+    if (window.location.href.indexOf("about") > -1) {
         document.getElementById("navbar-about").classList.add('is-active');
     }
-    if(window.location.href.indexOf("dances") > -1) {
+    if (window.location.href.indexOf("dances") > -1) {
         document.getElementById("navbar-dances").classList.add('is-active');
     }
 </script>
