a5aa517769ad19044bde1861c8a6d6994df08baa
wmjaynes <will@jaynes.org>
2019-06-15 12:58:33 -0400

    Add an error page for missing directions file.

--- app/Http/Controllers/DirectionsController.php
+++ app/Http/Controllers/DirectionsController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use function explode;
+use function file_exists;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use function preg_replace_array;
 use function strpos;
+use function view;
 
 class DirectionsController extends Controller
 {
@@ -24,6 +26,9 @@ class DirectionsController extends Controller
 //        $exists = Storage::disk()->exists($filename);
 
         $fname = Storage::disk()->getDriver()->getAdapter()->applyPathPrefix($filename);
+
+        if (! file_exists($fname))
+            return view('errors.missing-directions');
 
 //        dd($fname);
 
---
+++ resources/views/errors/missing-directions.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <section class="hero is-white is-rounded">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Cast and lead. That again.
+                </h1>
+                <h2 class="subtitle">
+                    Sorry. We thought that file was in place, but it isn't.
+                </h2>
+            </div>
+        </div>
+    </section>
+@endsection
